a19a2e55aa4f558a99a79499bb196bd1a5c55550
Mirna <mirnarecinos382@gmail.com>
2023-11-30 23:28:49 +0530

    Cambios en el body(Buttons)

--- js/script.js
+++ js/script.js
@@ -3,30 +3,7 @@
 
 document.querySelector('#app').innerHTML = `
 <div class="container" id="container">
-<section class="card mb-11 mx-auto cards" style="max-width: 800px;">
-  <div class="row ">
-    <div class="col-md-4 ">
-      <img src="https://res.cloudinary.com/dzb6jy2uq/image/upload/v1701202202/1_vcqitj.webp" class="img-fluid  ">
-    </div>
-    <div class="col-md-8">
-      <div class="card-body">
-        <h5 class="card-title">Laptop Lenovo Ideapad 3 AMD Ryzen 5-5500U 8GB RAM + 512GB SSD</h5><br>
-        Crea una lista 
-        <ul>
-          <li>Procesador: AMD Ryzen 5-5500U</li>
-          <li>Memoria de almacenamiento: 512GB SSD</li>
-          <li>Memoria RAM: 8GB</li>
-          <li>Tipo de Disco: SSD</li>
-          <li>Tamaño de pantalla: 15.6" Pulgadas</li>
-        </ul>
-        <h5 class="precio">$529.00</h5>
-        <div class="text-center"> <!-- Centra el boton -->
-        <button type="button" class="btn btn-outline-success" id="soldBtn"  onclick="changeButton()" >Añadir al carrito</button>
-        <div class="text-center">
-      </div>
-    </div>
-  </div>
-</section>
+
 
 
 
@@ -48,7 +25,132 @@ document.querySelector('#app').innerHTML = `
         </ul>
         <h5 class="precio">$379.00</h5>
         <div class="text-center"> <!-- Centra el boton -->
-        <button type="button" class="btn btn-outline-success"  id="soldBtn" onclick="changeButton()">Añadir al carrito</button>
+        <button type="button" class="btn btn-outline-success"  id="soldBtn" onclick="changeButton(this)">Añadir al carrito</button>
+        <div class="text-center">
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="card mb-11 mx-auto" style="max-width: 800px;">
+  <div class="row">
+    <div class="col-md-4 ">
+      <img src="https://res.cloudinary.com/dzb6jy2uq/image/upload/v1701397317/3_oh5e3u.webp" class="img-fluid  ">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Laptop HP gaming Victus Intel Core i5-12450H 16GB RAM + 512GB SSD + Nvidia RTX 3050</h5><br>
+      <!--   Crea una lista  -->
+        <ul>
+          <li>Procesador: Intel Core i5-12450H</li>
+          <li>Memoria de almacenamiento: 512GB SSD</li>
+          <li>Memoria RAM: 16 GB</li>
+          <li>Tipo de Disco: SSD</li>
+          <li>Tamaño de pantalla: 15.6" pulgadas</li>
+        </ul>
+        <h5 class="precio">$1,099.00</h5>
+        <div class="text-center"> <!-- Centra el boton -->
+        <button type="button" class="btn btn-outline-success"  id="soldBtn" onclick="changeButton(this)">Añadir al carrito</button>
+        <div class="text-center">
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="card mb-11 mx-auto cards" style="max-width: 800px;">
+  <div class="row ">
+    <div class="col-md-4 ">
+      <img src="https://res.cloudinary.com/dzb6jy2uq/image/upload/v1701202202/1_vcqitj.webp" class="img-fluid  ">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Laptop Lenovo Ideapad 3 AMD Ryzen 5-5500U 8GB RAM + 512GB SSD</h5><br>
+       
+        <ul>
+          <li>Procesador: AMD Ryzen 5-5500U</li>
+          <li>Memoria de almacenamiento: 512GB SSD</li>
+          <li>Memoria RAM: 8GB</li>
+          <li>Tipo de Disco: SSD</li>
+          <li>Tamaño de pantalla: 15.6" Pulgadas</li>
+        </ul>
+        <h5 class="precio">$529.00</h5>
+        <div class="text-center"> <!-- Centra el boton -->
+        <button type="button" class="btn btn-outline-success" id="soldBtn"  onclick="changeButton(this)" >Añadir al carrito</button>
+        <div class="text-center">
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="card mb-11 mx-auto cards" style="max-width: 800px;">
+  <div class="row ">
+    <div class="col-md-4 ">
+      <img src="https://res.cloudinary.com/dzb6jy2uq/image/upload/v1701397864/4_ixo4ri.webp" class="img-fluid  ">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Laptop Lenovo 2 en 1 AMD Ryzen 7-5700u 12GB RAM + 512GB SSD ROM</h5><br>
+     
+        <ul>
+          <li>Procesador: AMD Ryzen 7 5700U</li>
+          <li>Memoria de almacenamiento: 512 GB SSD</li>
+          <li>Memoria RAM: 12 GB</li>
+          <li>Tipo de Disco: SSD</li>
+          <li>Tamaño de pantalla: 15.6" Pulgadas</li>
+        </ul>
+        <h5 class="precio">$949.00</h5>
+        <div class="text-center"> <!-- Centra el boton -->
+        <button type="button" class="btn btn-outline-success" id="soldBtn"  onclick="changeButton(this)" >Añadir al carrito</button>
+        <div class="text-center">
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="card mb-11 mx-auto cards" style="max-width: 800px;">
+  <div class="row ">
+    <div class="col-md-4 ">
+      <img src="https://res.cloudinary.com/dzb6jy2uq/image/upload/v1701399857/6_tu5j10.webp" class="img-fluid  ">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Laptop Dell Inspiron 3520 Intel Core i7-1255u 8GB RAM + 512GB SSD</h5><br>
+       
+        <ul>
+          <li>Procesador: Intel Core i7-1255U</li>
+          <li>Memoria de almacenamiento: 512GB SSD</li>
+          <li>Memoria RAM: 8 GB</li>
+          <li>Tipo de Disco: SSD</li>
+          <li>Tamaño de pantalla: 15.6" pulgadas</li>
+        </ul>
+        <h5 class="precio">$947.00</h5>
+        <div class="text-center"> <!-- Centra el boton -->
+        <button type="button" class="btn btn-outline-success" id="soldBtn"  onclick="changeButton(this)" >Añadir al carrito</button>
+        <div class="text-center">
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="card mb-11 mx-auto cards" style="max-width: 800px;">
+  <div class="row ">
+    <div class="col-md-4 ">
+      <img src="https://res.cloudinary.com/dzb6jy2uq/image/upload/v1701400551/5_trviva.webp" class="img-fluid  ">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Laptop Asus Vivobook AMD Ryzen 5-5600H 8GB RAM + 256GB SSD ROM</h5><br>
+     
+        <ul>
+          <li>Procesador: AMD Ryzen 5 5600H</li>
+          <li>Memoria de almacenamiento: 256GB</li>
+          <li>Memoria RAM: 8 GB</li>
+          <li>Tipo de Disco: SSD</li>
+          <li>Tamaño de pantalla: 15.6" pulgadas</li>
+        </ul>
+        <h5 class="precio">$550.00</h5>
+        <div class="text-center"> <!-- Centra el boton -->
+        <button type="button" class="btn btn-outline-success" id="soldBtn"  onclick="changeButton(this)" >Añadir al carrito</button>
         <div class="text-center">
       </div>
     </div>
@@ -67,30 +169,54 @@ document.addEventListener("DOMContentLoaded", () => {
     // Agrega un evento de clic al botón
     /* Todos los botones muestran la misma alerta */
     soldButton.forEach((button) => {
+      let added = false; //Controla el estado del boton
+
         button.addEventListener("click", () => {
-              // Muestra una alerta al hacer clic en el botón
+
+          if(!added){
+              // Muestra alertas distintas(según el estado del boton) al hacer clic en el botón
               Swal.fire({
      
                 text: "Se ha añadido un nuevo producto a tu carrito.",
                 imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlTp7S4vNNnfo-V1vZFwbBB2H0EtX0aPcDxjs60XhmQf8veX1yCjoRpRaspEZZGG70WhM&usqp=CAU",
                 imageWidth: 200,
                 imageHeight: 150,
-                confirmButtonText: "Cerrar", 
+                confirmButtonText: "Aceptar", 
                })
-
-               changeButton(button);
-            })
-     
-        });
-
-        function changeButton(button) {
-           
-            button.innerHTML = 'Añadido al carrito'; // Cambiar el texto del botón
-            button.classList.remove('btn-outline-success');//Remover la clase (color del botón)
-            button.classList.add('btn-danger'); //Añadir nuevo color al hacer clic sobre el botón
-        }
-        
+               added = true
+              }
+              else {
+                Swal.fire({
     
+                  text: "¿Deseas retirar este producto de tu carrito?",
+                  imageUrl: "https://cdn-icons-png.flaticon.com/512/5412/5412816.png",
+                  imageWidth: 200,
+                  imageHeight: 150,
+                  showCancelButton: true,
+                  confirmButtonText: "Aceptar",
+                  cancelButtonText: "Cancelar",
+                  reverseButtons: true
+                }) .then((result) => {
+                  if(result.isConfirmed){
+                    added = false
+                    changeButton(button, false)
+                  }
+                })
+              }
+              if(added){
+              changeButton(button, true)
+              }
+            })
+        });
+        //Función para cambiar el texto y el color al boton al hacer clic
+        function changeButton(button, added) {
+ 
+           button.innerHTML = added ? "Añadido al carrito" : "Añadir al carrito"
+           button.classList.toggle('btn-outline-success', !added);
+           button.classList.toggle('btn-danger', added);
+              
+        }
+
     });
       
   
